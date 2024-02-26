b8d504ca207607466c11404cb532b7a5eecaaf7f
Ashtin Walter <113695788+AshtinJW-Dev@users.noreply.github.com>
2024-02-26 15:22:12 +0200

    finishing updates

---
+++ portfolio/src/data.js
@@ -0,0 +1,74 @@
+
+
+export const projects = [
+    {
+      title: "Kwantu Guesthouses",
+      subtitle: "Web Development & Digital Marketing",
+      description:
+        "In bringing the vision of Kwantu to life, I gained invaluable experience and knowledge from Mama Cynthia Nocwaka. After successfully re-doing the Kwantu Website, I have also been brought on to run their social media.",
+      image: 'images/projects/kwantu collage.png',
+      link: "https://kwantuguesthouses.capetown/",
+      
+    },
+    {
+      title: "Crater Bush Villa",
+      subtitle: "Web Development & Graphic Design",
+      description:
+        "Crater Bush Villa was an amazing project that taught me a lot about development and design. It was one of my biggest projects and the fact that it was a multi-page website is the reason I learned React.",
+      image: "images/projects/crater collage.png",
+      link: "https://craterbushvilla.co.za/",
+    },
+    {
+      title: "The Kushader Co.",
+      subtitle: "Web Development, Digitial Marketing, Graphic Design",
+      description:
+        "The TKCO gave me experience in digital marketing, graphic design and web dev. I ran their social media and created content, designed packaging for their entire range of products and was able to learn about CMS platforms when my expertise was needed to make improvements on their Wix site.",
+      image: "images/projects/kushader collage.png",
+      link: "https://www.thekushaderco.co.za/",
+    },
+    {
+      title: "Ngena Ekasi Tours",
+      subtitle: "Web Development, Digitial Marketing, Graphic Design",
+      description:
+        "Ngena Ekasi Tours was one of my first projects. I gained experience in web dev, graphic design and digital marketing, all while learning invaluable lessons in business. ",
+      image: "images/projects/ngena collage.png",
+      link: "https://ngenaekasitours.co.za/",
+    },
+    {
+      title: "To-Do List",
+      subtitle: "Personal Project",
+      description:
+        "A personal project built using HTML, CSS and JS. It's a To-Do List app that allows you to create lists and tasks and stores them in the cache.",
+      image: "images/projects/to do collage.png",
+      link: "https://github.com/AshtinJW-Dev/ToDoList",
+    },
+    
+  ];
+
+  export const skills = [
+    'React',
+    'JavaScript',
+    'CSS',
+    'Bootstrap & Tailwind',
+    'HTML',
+    'PHP',
+    'Node.js',
+    'Graphic Design & Digital Marketing'
+    // Add more skills as needed
+  ];
+
+  export const testimonials = [
+    { 
+      quote:'"I thank God for introducing you to me, brother. Glory be to him."', 
+      image:'images/shabooya.jpg', 
+      name:'Thandisizwe Dhano',
+      company:'Ngena Ekasi Tours'
+    },
+      { 
+      quote:'"We will carry your name high."', 
+      image:'images/shabooya.jpg', 
+      name:'Lana Rodel',
+      company:'Crater Bush Villa'
+    }
+
+  ];
